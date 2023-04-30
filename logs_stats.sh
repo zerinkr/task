2b15130a3d4ed3ddb8bb801969c19d08bdee21e8
zerinkr <132204261+zerinkr@users.noreply.github.com>
2023-04-30 21:55:38 +0200

    Final

--- logs_stats.sh
+++ logs_stats.sh
@@ -5,9 +5,8 @@ if [ -z "$BASH_VERSION" ]; then
   echo "This script must be executed by bash shell"
   exit 1
 fi
-
 # Instrukcije - bez argumenata
-if [ $# -eq 0 ]; then
+if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <logs_directory_path> <search_string>"
   exit 1
 fi
@@ -19,15 +18,21 @@ if [ ! -d "$logs_dir" ]; then
   exit 1
 fi
 
-# String za search x2
-search_string=$2
+# String za search x2 - provjeri
+if [ -z "$2" ]; then
+  echo "Error: search string argument not provided"
+  exit 1
+fi
+
+# search string
+search_string="$2"
 
 # Generalno o fajlu pa novi red
 printf "%-20s%-20s%-20s%-20s\n" "File Name" "File Size" "Total Lines" "Search Word Count"
 printf "%s\n" "-----------------------------------------------------------------------"
 
 # awk da skenira fajl
-for file in "$logs_dir"/*.log; do
+while IFS= read -r -d '' file; do
   if [ -f "$file" ]; then
     # Get file stats
     file_name=$(basename "$file")
@@ -36,9 +41,7 @@ for file in "$logs_dir"/*.log; do
     search_word_count=$(grep -c "$search_string" "$file")
 
 # rezultat
-    printf "%-20s%-20s%-20s%-20s\n" "$file_name" "$file_size" "$total_lines" "$search_word_count"
+ printf "%-20s%-20s%-20s%-20s\n" "$file_name" "$file_size" "$total_lines" "$search_word_count"
   fi
-done | sort -rnk4
-
+done < <(find "$logs_dir" -type f -name '*.log' -print0) | sort -rnk4
 exit 0
-
